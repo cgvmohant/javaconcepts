575f4e3f66218857f5c01ed21f8bcf2c701aa3c7
cgvmohant <cgvmohant@gmail.com>
2023-09-10 09:02:35 +0530

    First commit

---
+++ src/datatypes/Basics1.java
@@ -0,0 +1,17 @@
+package datatypes;
+
+public class Basics1 {
+
+	public static void main(String[] args) 
+	{
+		byte b1=10;
+		byte b2=-10;
+		byte b3 = -128;
+		byte b4 = 127;
+		byte b5 = 130;
+		
+		
+		
+	}
+
+}
