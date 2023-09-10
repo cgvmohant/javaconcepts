5cffa05a61fdd3a6087c1af3f35bb2167fd26103
cgvmohant <cgvmohant@gmail.com>
2023-09-10 11:59:11 +0530

    Added page classes and related tests

---
+++ src/tests/HomeTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class HomeTest {
+	public static void main(String[] args) {
+
+
+		System.out.println("This is the home test");
+		
+	}
+}
---
+++ src/tests/LoginTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("This is the login test");
+		
+	}
+}
